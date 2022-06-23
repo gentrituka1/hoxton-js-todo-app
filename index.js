f9d856783b73a0e0b35ee7ba43c621faa09872fb
Gentrit Uka <gentrit-uka@live.com>
2022-06-23 14:39:42 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+// - Start this project from scratch, write whatever HTML, CSS & JS you need
+// - Create a state object
+// - Create action functions that update state
+// - Create render functions that read from state
+
+let state = {
+    todos: [
+        { title: "Go shopping", completed: false },
+        { title: "Go to the gym", completed: false },
+        { title: "Go to the movies", completed: false },
+    ]
+    
+}
+
+function render (){
+
+
+
+}
+
+render();
+
