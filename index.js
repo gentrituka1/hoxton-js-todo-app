55ebfb6b6ce4d8c2633192617921be78c9657da4
Gentrit Uka <gentrit-uka@live.com>
2022-06-23 15:19:59 +0200

    updated

--- index.js
+++ index.js
@@ -3,19 +3,81 @@
 // - Create action functions that update state
 // - Create render functions that read from state
 
-let state = {
-    todos: [
-        { title: "Go shopping", completed: false },
-        { title: "Go to the gym", completed: false },
-        { title: "Go to the movies", completed: false },
-    ]
+let state = [
+    { title: "Go shopping", completed: false },
+    { title: "Go to the gym", completed: false },
+    { title: "Go to the movies", completed: false },
+]
     
-}
 
-function render (){
+function render(){
+   document.body.textContent = ""
 
+   let containerDiv = document.createElement("div")
 
+   let firstH3Element = document.createElement("h3")
+    firstH3Element.textContent = "OPTIONS"
+    firstH3Element.className = "header-3"
 
+    let checkboxDiv = document.createElement("div")
+    checkboxDiv.className = "checkbox-div"
+
+    let pEl1 = document.createElement("p")
+    pEl1.textContent = "Show completed"
+
+    let checkbox = document.createElement("input")
+    checkbox.type = "checkbox"
+    checkbox.name = "checkbox"
+
+    let secondH3Element = document.createElement("h3")
+    secondH3Element.textContent = "ADD ITEM"
+    secondH3Element.className = "header-3"
+
+    let addItemDiv = document.createElement("div")
+    addItemDiv.className = "add-item-div"
+
+    let addItemInput = document.createElement("input")
+    addItemInput.type = "text"
+    addItemInput.name = "add-item"
+
+    let pEl2 = document.createElement("p")
+    pEl2.textContent = "Add item"
+
+    let thirdH3Element = document.createElement("h3")
+    thirdH3Element.textContent = "TODO"
+    thirdH3Element.className = "header-3"
+
+    let todoListDiv = document.createElement("div")
+    todoListDiv.className = "todo-list-div"
+
+    let todoListUl = document.createElement("ul")
+    todoListUl.className = "todo-list"
+
+    pEl2.addEventListener("click", function(){
+        let inputValue = addItemInput.value
+            let newTodo = {
+                title: inputValue,
+                completed: false}
+            state.todos.push(newTodo)
+            render()
+    })
+
+    for (let element of state){
+    let todoListLi = document.createElement("li")
+    todoListLi.className = "todo-items"
+
+    let todoItemsInput = document.createElement("input")
+    todoItemsInput.type = "checkbox"
+    todoItemsInput.name = "checkbox"
+
+    let todoItemsSpan = document.createElement("span")
+    todoItemsSpan.textContent = element.name
+
+    let todoItemsDelete = document.createElement("button")
+    todoItemsDelete.textContent = "Delete"
+
+    todoListLi.append(todoItemsInput, todoItemsSpan, todoItemsDelete)
+    }
 }
 
 render();
